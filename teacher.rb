7c31f815f15ad62d080ead5c1eb4412cab00b5ee
Eric <eric2umeh@yahoo.com>
2023-04-10 17:25:27 +0100

    Update student.rb & teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: parent_permission)
+  def initialize(age, specialization, name = 'Unknown', _parent_permission: parent_permission)
     super(age, name, parent_permission)
     @specialization = specialization
   end
