5c55b9565cd1f93681b763ac7a5baba1ed4cbf32
Eric <eric2umeh@yahoo.com>
2023-04-18 12:51:24 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './teacher'
 require './rental'
 require './classroom'
 require './nameable'
+require './preserve_data'
 
 class App
   def initialize
@@ -14,29 +15,38 @@ class App
   end
 
   def list_all_books
+    @book = load_data('books.json')
     if @book.empty?
       puts 'No books available'
     else
-      @book.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+      data = @book.map(&:to_h)
+      data.each do |book|
+        puts "Title: #{book['title']}, Author: #{book['author']}"
+      # @book.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
     end
   end
 
   def list_all_people
+    @people = load_data('people.json')
     if @people.empty?
       puts 'No people available! Please add people'
     else
-      @people.each_with_index.map do |person, index|
-        if person.instance_of?(Student)
-          str1 = "[#{index}] - [#{person.class}] - Id: #{person.id} , "
-          str2 =
-            " Name: #{person.name}, Age: #{person.age}"
-        else
-          str1 = "[#{index}] - [#{person.class}] - Id: #{person.id} ,"
-          str2 =
-            "  Name: #{person.name}, Age:  #{person.age}, Specialization: #{person.specialization}\n"
-        end
-        puts str1 + str2
+      data = @people.map(&:to_h)
+      data.each_with_index.map do |person, index|
+      data.each do |person|
+        puts "Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']}"
       end
+      #   if person.instance_of?(Student)
+      #     str1 = "[#{index}] - [#{person.class}] - Id: #{person.id} , "
+      #     str2 =
+      #       " Name: #{person.name}, Age: #{person.age}"
+      #   else
+      #     str1 = "[#{index}] - [#{person.class}] - Id: #{person.id} ,"
+      #     str2 =
+      #       "  Name: #{person.name}, Age:  #{person.age}, Specialization: #{person.specialization}\n"
+      #   end
+      #   puts str1 + str2
+      # end
     end
   end
 
@@ -61,6 +71,7 @@ class App
 
     student = Student.new(age, name, parents_permission)
     @people.push(student)
+    save_data(@people, 'people.json')
     puts 'Student created successfully'
   end
 
@@ -74,6 +85,7 @@ class App
 
     teacher = Teacher.new(age, name, specialization)
     @people.push(teacher)
+    save_data(@people, 'people.json')
     puts 'Teacher created successfully'
   end
 
@@ -84,18 +96,21 @@ class App
     author = gets.chomp
 
     book = Book.new(title, author)
-    @book.push(book)
+    @book. << book
+    save_data(@book, 'books.json')
     puts 'Book created successfully'
   end
 
   def create_rental
     puts 'Select a book from the following list by number'
+    @book = load_data('books.json')
     @book.each_with_index do |book, index|
       puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
     end
     rental_book = gets.chomp.to_i
 
     puts 'Select a person from the following list by number'
+    @people = load_data('people.json')
     @people.each_with_index do |person, index|
       puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
@@ -106,15 +121,19 @@ class App
 
     rental = Rental.new(date, @book[rental_book], @people[rental_person])
     @rental.push(rental)
+    save_data(@rental, 'rental.json')
     puts 'Rental created successfully'
   end
 
   def list_all_rentals
+    @rental = load_data('rental.json')
     print 'ID of person: '
     id = gets.chomp.to_i
     puts 'Rentals:'
     @rental.each do |rental|
-      puts "Date: #{rental.date}, Book: \"#{rental.book.title}\", by #{rental.book.author}" if id == rental.person.id
+      if id == rental['person']['id']
+        puts "Date: #{rental.date}, Book: \"#{rental.book.title}\", by #{rental.book.author}"
+      end
     end
   end
 end
