d59bce8a407477d280249e610ab1d322f19319d9
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-19 16:17:45 +0300

    Add trimmer_decorator tests

---
+++ spec/trimmer_decorator_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe TrimmerDecorator do
+  let(:person) { Person.new(18, 'John Doe') }
+  let(:decorator) { TrimmerDecorator.new(person) }
+
+  describe '#correct_name' do
+    context 'when name is longer than 10 characters' do
+      it 'should return a trimmed version of name' do
+        person.name = 'This is a very long name'
+        expect(decorator.correct_name).to eq('This is a ')
+      end
+    end
+    context 'when name is shorter than or equal to 10 characters' do
+      it 'should return the original name' do
+        person.name = 'John Doe'
+        expect(decorator.correct_name).to eq('John Doe')
+      end
+    end
+  end
+  describe '#initialize' do
+    it 'should set nameable attribute to the input object' do
+      expect(decorator.nameable).to eq(person)
+    end
+  end
+end
