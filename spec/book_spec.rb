486f37f79a978bb389c3ee3f4b0d7e3cc1aa9eb4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-19 16:32:08 +0300

    Revert book tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,25 +1,30 @@
-require_relative '../book.rb'
-require_relative '../rental.rb'
-require_relative '../student.rb'
+require 'spec_helper'
 
-describe Book do 
-    context 'When testing the Book class' do
-        before :each do
-            @book = Book.new('Davinci Code', 'Dan Brown')
-            @person = Person.new(age:'25', name:'Howard', parent_permission:true)
-            @mock_rental = Rental.new(date: '2020-01-01', book: @book, person: @person)
-        end
+describe Book do
+  let(:book) { Book.new('The Great Gatsby', 'F. Scott Fitzgerald') }
 
-        it 'it is initialized as an instance of Book' do
-          expect(@book).to be_an_instance_of Book
-          expect(@book).to have_attributes(title: 'Davinci Code') 
-          expect(@book).to have_attributes(author: 'Dan Brown')
-        end
-
-        it 'adds a rental to the rentals array when called' do
-            @book.add_rental(@mock_rental)
-            expect(@book).to respond_to('add_rental')
-            expect(@book.rentals[0]).to eq @mock_rental
-        end
+  describe '#initialize' do
+    it 'should set the title and author' do
+      expect(book.title).to eq('The Great Gatsby')
+      expect(book.author).to eq('F. Scott Fitzgerald')
     end
+
+    it 'should initialize an empty array of rentals' do
+      expect(book.rentals).to be_an(Array)
+      expect(book.rentals).to be_empty
+    end
+  end
+
+  describe '#add_rental' do
+    it 'should add the rental to the rentals array' do
+      book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+      rental = { date: '2022-01-01', book: book.to_h, person: { name: 'John Doe', email: 'johndoe@example.com' } }
+
+      expect do
+        book.add_rental(rental)
+      end.to change { book.rentals.length }.by(1)
+
+      expect(book.rentals.last).to eq rental
+    end
+  end
 end
